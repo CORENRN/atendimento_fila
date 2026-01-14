4a8f33eaa27015b3aa17631b0b179c73760a34c6
viictusilva@gmail.com <viictusilva@gmail.com>
2026-01-14 15:13:09 -0300

    Inputupdate

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-2 border-primary bg-lightW py-3 dark:text-gray-300 focus:border-[#3368d1]  focus:ring-[#3368d1] rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-2 border-primary bg-lightW py-3 focus:border-[#3368d1]  focus:ring-[#3368d1] rounded-md shadow-sm']) }}>
