31d751572574d0f9b8156ff418bedeee8530fb0a
viictusilva@gmail.com <viictusilva@gmail.com>
2026-01-21 14:45:57 -0300

    Upload

--- resources/views/ticket_take.blade.php
+++ resources/views/ticket_take.blade.php
@@ -51,7 +51,7 @@
                     </p>
                 </button>
 
-            
+                <!-- Botão Preferencial -->
                 <button 
                     type="submit" 
                     name="type" 
